8c1ed5f8a0c6ee3c3a9ff4537aafd08a79c20f34
lflinhares <lf.dig@hotmail.com>
2025-10-27 21:41:35 -0300

    refining cleanup frequency

--- src/download/cleanup.service.ts
+++ src/download/cleanup.service.ts
@@ -8,35 +8,39 @@ export class CleanupService {
   private readonly logger = new Logger(CleanupService.name);
   private readonly downloadsDir = path.join(process.cwd(), 'downloads');
 
-  @Cron(CronExpression.EVERY_5_MINUTES)
+  @Cron(CronExpression.EVERY_MINUTE)
   handleCron() {
     this.logger.log('Iniciando tarefa de limpeza da pasta de downloads...');
-    
+
     fs.readdir(this.downloadsDir, (err, files) => {
       if (err) {
         this.logger.error('Não foi possível ler a pasta de downloads', err);
         return;
       }
-      
+
       if (files.length === 0) {
         this.logger.log('Nenhum arquivo para limpar.');
         return;
       }
 
-      files.forEach(file => {
+      files.forEach((file) => {
         const filePath = path.join(this.downloadsDir, file);
-        
+
         fs.stat(filePath, (err, stats) => {
           if (err) {
-            this.logger.error(`Não foi possível obter informações do arquivo ${file}`, err);
+            this.logger.error(
+              `Não foi possível obter informações do arquivo ${file}`,
+              err,
+            );
             return;
           }
 
           const now = new Date().getTime();
-          const fileAgeInMinutes = (now - new Date(stats.mtime).getTime()) / 60000;
-          
+          const fileAgeInMinutes =
+            (now - new Date(stats.mtime).getTime()) / 60000;
+
           // Apaga arquivos com mais de 10 minutos
-          if (fileAgeInMinutes > 10) {
+          if (fileAgeInMinutes > 5) {
             fs.unlink(filePath, (err) => {
               if (err) {
                 this.logger.error(`Falha ao deletar o arquivo ${file}`, err);
